ee567827bffe1f61cf5b7b86c55b3ea75f352cfc
daniel alexander vasquez cazon <a2019115093@estudiantes.upsa.edu.bo>
2020-12-05 16:19:54 -0400

    Se agrego el modelo y el controlador

---
+++ PreparatoriaA/PreparatoriaA/Models/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.Entity;
+
+namespace PreparatoriaA.Models
+{
+    public class DataContext : DbContext
+    {
+
+        public DataContext() : base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<PreparatoriaA.Models.Student> Students { get; set; }
+    }
+}
---
+++ PreparatoriaA/PreparatoriaA/Models/Student.cs
@@ -0,0 +1,29 @@
+﻿
+
+namespace PreparatoriaA.Models
+{
+    using System.ComponentModel.DataAnnotations;
+    public class Student
+    {
+        public enum Lista
+        {
+            ingenieria = 10,
+            humanidades = 20,
+            artes = 30
+        }
+        [Key]
+        public int EstudianteID { get; set; }
+
+        [StringLength(20)]
+        [Required]
+        [Display(Name = "Nombre")]
+        public string EstudianteNombre { get; set; }
+
+        [StringLength(20)]
+        [Display(Name = "Apellido")]
+        public string EstudianteApellido { get; set; }
+
+        [Display(Name = "Facultad")]
+        public Lista EstudianteFacultad { get; set; }
+    }
+}
